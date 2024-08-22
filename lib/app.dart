26958687fd5777465e91e39fb988e219d0202d08
Mirko Gonzalez Vasquez <mirko.gonzalez@xintec.cl>
2024-08-22 12:08:55 -0400

    ci: step 1 config main page

--- lib/app.dart
+++ lib/app.dart
@@ -1,71 +1,60 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_bloc_freezed/bloc/counter_bloc.dart';
+import 'package:flutter/widgets.dart';
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => CounterBloc(),
-      child: BlocBuilder<CounterBloc, CounterState>(
-        builder: (context, state) {
-          return MaterialApp(
-            title: 'Flutter Bloc y Freezed',
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData.dark(),
-            home: Scaffold(
-              appBar: AppBar(
-                title: const Text('Flutter BloC + Freezed'),
-              ),
-              body: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Center(
-                    child: Text(
-                      state.when(
-                        initial: () => '0',
-                        updateValue: (value) => value.toString(),
-                      ),
-                      style: const TextStyle(fontSize: 44),
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          margin: const EdgeInsets.all(20),
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(backgroundColor: Colors.indigoAccent.shade400),
-                            onPressed: () {
-                              context.read<CounterBloc>().add(const CounterEvent.addOrSubstractValue(value: 1));
-                            },
-                            child: const Icon(Icons.add),
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: Container(
-                          margin: const EdgeInsets.all(20),
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(backgroundColor: Colors.indigoAccent.shade400),
-                            onPressed: () {
-                              context.read<CounterBloc>().add(const CounterEvent.addOrSubstractValue(value: -1));
-                            },
-                            child: const Icon(Icons.remove),
-                          ),
-                        ),
-                      ),
-                    ],
-                  )
-                ],
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter_bloc + freezed'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Center(
+              child: Text(
+                '0',
+                style: TextStyle(fontSize: 44),
               ),
             ),
-          );
-        },
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.all(20),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(backgroundColor: Colors.indigoAccent.shade200),
+                      onPressed: () {
+                        // Aquí llamaremos a la función del BloC
+                      },
+                      child: const Icon(Icons.add),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.all(20),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(backgroundColor: Colors.indigoAccent.shade200),
+                      onPressed: () {
+                        // Aquí llamaremos a la función del BloC
+                      },
+                      child: const Icon(Icons.delete),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
